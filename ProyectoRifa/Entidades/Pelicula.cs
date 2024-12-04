f181eeef0f3768eed2a5b75121bdec4e5be60e57
Leire Bozal <leire.bozalce@elorrieta-errekamari.com>
2024-12-04 11:46:13 +0100

    cambios

---
+++ ProyectoRifa/Entidades/Pelicula.cs
@@ -0,0 +1,8 @@
+﻿namespace ProyectoRifa.Entidades
+{
+    public class Pelicula
+    {
+        public string Titulo { get; set; }
+        public DateTime Lanzamiento { get; set; }
+    }
+}
---
+++ ProyectosMaui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ProyectosMaui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
